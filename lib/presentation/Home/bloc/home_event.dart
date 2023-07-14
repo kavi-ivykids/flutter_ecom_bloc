02ff1f24cfa64c54b72879357bdf5a66e3440cb9
kavi-ivykids <136069340+kavi-ivykids@users.noreply.github.com>
2023-07-14 17:11:47 +0530

    added events and states to home

--- lib/presentation/Home/bloc/home_event.dart
+++ lib/presentation/Home/bloc/home_event.dart
@@ -2,3 +2,11 @@ part of 'home_bloc.dart';
 
 @immutable
 abstract class HomeEvent {}
+
+class HomeProductWishListButtonClickedEvent extends HomeEvent {}
+
+class HomeProductCartButtonClickedEvent extends HomeEvent {}
+
+class HomeWishListButtonNavigateEvent extends HomeEvent {}
+
+class HomeCartButtonNavigateEvent extends HomeEvent {}
