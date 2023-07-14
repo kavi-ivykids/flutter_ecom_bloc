19d42694d266db3d78a885720bf3dbcf77cd2b9f
kavi-ivykids <136069340+kavi-ivykids@users.noreply.github.com>
2023-07-14 13:41:33 +0530

    initial commit

---
+++ lib/data/raw_data.dart
@@ -0,0 +1,50 @@
+class DressData {
+  static List<Map<String, dynamic>> dressData = [
+    {
+      "id": 1,
+      "name": "Floral Print Maxi Dress",
+      "description": "A beautiful floral print maxi dress perfect for summer.",
+      "price": 49.99,
+      "color": "blue",
+      "size": ["S", "M", "L", "XL"],
+      "imageUrl":
+          "https://www.aarke.in/media/catalog/product/i/m/img_7757-edit.jpg",
+      "images": [
+        "https://example.com/images/dress1_front.jpg",
+        "https://example.com/images/dress1_back.jpg",
+        "https://example.com/images/dress1_side.jpg"
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Off-Shoulder Bodycon Dress",
+      "description":
+          "An elegant off-shoulder bodycon dress for special occasions.",
+      "price": 79.99,
+      "color": "black",
+      "size": ["XS", "S", "M", "L"],
+      "imageUrl":
+          "https://assets.ajio.com/medias/sys_master/root/20230629/hRRf/649cf089a9b42d15c9201ab2/-1117Wx1400H-466080147-blue-MODEL.jpg",
+      "images": [
+        "https://example.com/images/dress2_front.jpg",
+        "https://example.com/images/dress2_back.jpg",
+        "https://example.com/images/dress2_side.jpg"
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Striped Midi Dress",
+      "description": "A stylish striped midi dress with adjustable straps.",
+      "price": 59.99,
+      "color": "white",
+      "size": ["S", "M", "L"],
+      "imageUrl":
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf0MdnBkMTsg8BAXqtohKtCeyDHzzV1wrhLA&usqp=CAU",
+      "images": [
+        "https://example.com/images/dress3_front.jpg",
+        "https://example.com/images/dress3_back.jpg",
+        "https://example.com/images/dress3_side.jpg"
+      ]
+    },
+  ];
+}
